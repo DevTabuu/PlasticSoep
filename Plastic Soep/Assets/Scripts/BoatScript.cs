3fb25e0955dee22ebc040694debe6a4773b17272
Rick van Sloten <rickvansloten@gmail.com>
2018-11-26 13:29:21 +0100

    Folders and Scenes

---
+++ Plastic Soep/Assets/Scripts/BoatScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatScript : MonoBehaviour {
+
+    private float _speed;
+
+
+
+    private void Start () {
+	}
+
+    private void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
