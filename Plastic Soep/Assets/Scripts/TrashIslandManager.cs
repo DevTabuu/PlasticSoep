07425b48f8439e1fd91e626a912646a6ea89a1f1
Dryomc <521251@student.glu.nl>
2018-12-03 13:15:38 +0100

    Begin garbage island scirpt

---
+++ Plastic Soep/Assets/Scripts/TrashIslandManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashIslandManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _trashIslandPrefab;
+
+    [SerializeField]
+    float _minX, _maxX, _minY, _maxY;
+
+    private float _timer;
+
+	private void Start () {
+        _timer = 0;
+	}
+	
+	private void Update () {
+        _timer += Time.deltaTime;
+
+        if(_timer >= 2f)
+        {
+            _timer = 0;
+            SpawnTrashIsland();
+        }
+	}
+
+    private void SpawnTrashIsland()
+    {
+        Instantiate(_trashIslandPrefab, new Vector3(Random.Range(_minX, _maxX), 0, Random.Range(_minY, _maxY)), Quaternion.identity);
+    }
+}
---
+++ Plastic Soep/Assets/Scripts/TrashIslandScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashIslandScript : MonoBehaviour {
+
+    private RectTransform _rectTransform ;
+
+    private void Start()
+    {
+    }
+
+    private void Update ()
+    {
+		if(transform.position.y < )
+        {
+            transform.position = Vector3.up;
+        }
+
+        Debug.Log(_bounds.max.y);
+	}
+}
