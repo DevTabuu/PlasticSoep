6bd72f516ba039a91e48931991526a6add871c6e
Rick van Sloten <rickvansloten@gmail.com>
2019-01-14 15:13:00 +0100

    Testerino

--- Plastic Soep/Assets/Scripts/Boats/CleaningBoat.cs
+++ Plastic Soep/Assets/Scripts/Boats/CleaningBoat.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CleaningBoat : Boat
 {
+    protected override void Start()
+    {
+        base.Start();
+        GameObject manager = GameObject.FindGameObjectWithTag("Manager");
+        DialogManager _dialogManager = manager.GetComponent<DialogManager>();
+        _dialogManager.ShowDialog(transform, "I am a cleaning boat; let me clean the garbage!", 5);
+    }
+
     public override bool CanCollect(Collectable collectable)
     {
         return collectable is TrashIsland;
--- Plastic Soep/Assets/Scripts/Boats/FishingBoat.cs
+++ Plastic Soep/Assets/Scripts/Boats/FishingBoat.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class FishingBoat : Boat {
 
+    protected override void Start()
+    {
+        base.Start();
+        GameObject manager = GameObject.FindGameObjectWithTag("Manager");
+        DialogManager _dialogManager = manager.GetComponent<DialogManager>();
+        _dialogManager.ShowDialog(transform, "I am a fishing boat; let me fish!", 5);
+    }
+
     public override bool CanCollect(Collectable collectable)
     {
         return collectable is FishingArea;
